70f3953ca2949f6e564915c5ea6433f14787dec4
Drishti Kirodiwal <84236817+Drishti2003@users.noreply.github.com>
2023-10-26 22:21:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number!🎉";
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".guess").value = 27;
+console.log(document.querySelector(".guess").value);
+*/
+
+document.querySelector(".check").addEventListener("click", function () {
+  console.log(document.querySelector(".guess").value);
+});
